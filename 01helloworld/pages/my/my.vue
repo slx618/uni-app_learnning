ba68cf08ad99f27feb9c4ded7c00e60f05df0112
slx618 <slx618@hotmail.com>
2021-09-26 23:55:59 +0800

    day1

--- 01helloworld/pages/my/my.vue
+++ 01helloworld/pages/my/my.vue
@@ -3,19 +3,14 @@
         <view class="page">
             {{c}}
         </view>
-        <text>xxxx</text>
+        <text>轮播图</text>
         <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
-            <swiper-item>
+            <swiper-item  v-for="img in imgList">
                 <view class="swiper-item">
-                    <text>mytext1</text>
+                    <text>{{img.txt}}</text>
                 </view>
             </swiper-item>
-            <swiper-item>
-                <view class="swiper-item">
-                    <text>mytext2</text>
-                    
-                </view>
-            </swiper-item>
+         
         </swiper>
 	</view>
     
@@ -25,9 +20,29 @@
 	export default {
 		data() {
 			return {
-				c:"这是我的内容"
+				c:"这是我的内容",
+                imgList: [
+                    {txt:"xx1", id:"1"},
+                    {txt:"xx2", id:"2"},
+                ]
 			}
-		},
+		},  
+        onLoad() {
+            // uni.request({
+            //     url:'',
+            //     method:'POST',
+            //     header:{
+            //         'content-type': 'application/x-www-form-urlencode'
+            //     },
+            //     data:{
+            //         qq:"x"
+            //     },
+            //     success: res => {
+            //         console.log(res)
+            //         this.imgList = res.data.data
+            //     }
+            // })
+        },
 		methods: {
 			
 		}
